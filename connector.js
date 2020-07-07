2c4753f3ef3893aff334461564b16b42bddb4f04
Max Shymchuk <octorix@mail.ru>
2020-07-07 23:31:16 +0300

    feat: add connector class

---
+++ connector.js
@@ -0,0 +1,28 @@
+export default class Connector {
+  constructor() {
+    this.connector = new XMLHttpRequest();
+    this.connector.onload = () => {
+      if (this.connector.status != 200) {
+        alert('Error');
+      } else {
+        this.response = this.connector.response;
+      }
+    }
+  }
+
+  loadWords() {
+    this.connector.open("GET", 'words.txt');
+    this.connector.send();
+  }
+
+  getWord(word) {
+    if (typeof word === 'string') {
+      req.open("GET", getDictionaryUrl(word));
+      req.send();
+    }
+  }
+}
+
+function a() {
+  alert('asdasd')
+}
--- script.js
+++ script.js
@@ -1,26 +1,15 @@
-const tokens = [
-  'dict.1.1.20200704T213006Z.cb2d5e00d0e31eed.3cd15300cbbd10dfa117c1044cf4d651bdf0d776',
-  'dict.1.1.20200704T213003Z.76af1209ced5b5ec.aacc04241ff0c42bfcc2062557567f0f96c9b6a4',
-  'dict.1.1.20200704T212057Z.909f5665d7d6acde.e9cde7ab66adc43ddb78c9a9d30a501049af5e0a'
-]
+import './connector';
+import * as config from './config';
+
+const connector = new Connector();
 
 window.onload = () => {
     document.getElementById('update').onclick = () => {
-        req.open("GET", getDictionaryUrl('тест'));
-        req.send();
-    }
-    document.getElementById('word').onclick = () => {
-        req.open("GET", 'russian.txt');
-        req.send();
+        connector.loadWords();
+        alert(JSON.stringify(connector.response));
     }
 }
 
-function getDictionaryUrl(word, key = tokens[0]) {
-    return `https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=${key}&lang=ru-ru&text=${word}`;
-}
-
-const req = new XMLHttpRequest();
-
-req.onload = () => {
-  alert(JSON.stringify(req.response));
+function getDictionaryUrl(word, token = tokens[0]) {
+    return `https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=${token}&lang=ru-ru&text=${word}`;
 }
